0d210347846df26d794d3f7ab11b197f836b9155
Meghana <meghana.meghanask.sk@gmail.com>
2021-12-04 08:19:23 +0000

    added pattern questions

--- Pattern-Questions/invertedTriangle.cpp
+++ Pattern-Questions/invertedTriangle.cpp
@@ -11,8 +11,11 @@ Print
 using namespace std;
 
 int main(){
-  for (int i = 0;i < 4;i++){
-    for(int j=0;j<4-i;j++){
+  int n;
+  cout << "enter n";
+  cin >> n;
+  for (int i = 0;i < n;i++){
+    for(int j=0;j<n-i;j++){
       cout << "*";
     }
     cout << endl;
